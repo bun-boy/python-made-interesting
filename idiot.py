b3ecc58a741151df3ae523345eade0b2ba21693e
bun-boy <byash.iitk@gmail.com>
2021-01-18 23:27:22 +0530

    idiot

---
+++ idiot.py
@@ -0,0 +1,8 @@
+import pyinputplus as pyip
+
+while True:
+    response = pyip.inputYesNo("Want to know how to keep an idiot busy for hours?")
+
+    if response == 'no': break
+
+print("I hope you figured one ;))")
